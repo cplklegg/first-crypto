699a96191f999694de0747afbd662ea0596e68db
cplKlegg <emcgrath@startmail.com>
2025-02-26 01:47:04 -0500

    preparing first-crypto for deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+const privateKeys = process.env.PRIVATE_KEYS || ""
+
+//import helpers from "@nomicfoundation/hardhat-toolbox/network-helpers";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.9",
+  networks: {
+    localhost: {},
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: privateKeys.split(",")
+    }
+  },
 };
